afc121dfa985ff0e1194c4cb65b09f7519079051
Papp Eva <ms.eva.papp@gmail.com>
2023-04-12 21:58:13 +0200

    TC1-2

---
+++ vizsgaremek_test_conduit/functions.py
@@ -0,0 +1,21 @@
+# TC_01 Adatkezelési nyilatkozat használata
+
+# TC_02 Regisztráció
+
+# TC_03 Bejelentkezés
+
+# TC_04 Adatok listázása
+
+# TC_05 Több oldalas lista bejárása
+
+# TC_06 Új adat bevitel
+
+# TC_07 Ismételt és sorozatos adatbevitel adatforrásból
+
+# TC_08 Meglévő adat módosítás
+
+# TC_09 Adat vagy adatok törlése
+
+# TC_10 Adatok lementése felületről
+
+# TC_11 Kijelentkezés
