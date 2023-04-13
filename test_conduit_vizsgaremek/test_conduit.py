08488df2756342db4966d68f700afce8480677b3
Papp Eva <ms.eva.papp@gmail.com>
2023-04-13 12:13:08 +0200

    one

--- test_conduit_vizsgaremek/test_conduit.py
+++ test_conduit_vizsgaremek/test_conduit.py
@@ -28,59 +28,75 @@ class TestConduit(object):
     def teardown_method(self):
         self.browser.quit()
 
-    # TC_01 Adatkezelési nyilatkozat használata
-    def test_kuki(self):
-        # id=cookie-policy-panel látható?
+# TC_01 Adatkezelési nyilatkozat használata
+    def test_cookie(self):
         accept_cookie_btn = self.browser.find_element(By.XPATH,
                                                       '//button [@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
         accept_cookie_btn.click()
-        # id=cookie-policy-panel látható? nem. OK.
-        # WebDriverWait(browser, 2).until_not(EC.presence_of_element_located((By.ID, 'cookie-policy-panel')))
+        # cookie_accept = self.browser.get_cookie("vue-cookie-accept-decline-cookie-policy-panel")
+        # assert cookie_accept["value"] == "accept"
         time.sleep(2)
 
-    # TC_02 Regisztráció tesztelése
-    def test_register(self):
-        register_btn = self.browser.find_element(By.LINK_TEXT, 'Sign up')
-        register_btn.click()
+        # try to find cookie panel
+        assert len(self.browser.find_elements(By.ID, 'cookie-policy-panel')) == 0
+
+# TC_02 Regisztráció tesztelése
+#     def test_register(self):
+#         register_btn = self.browser.find_element(By.LINK_TEXT, 'Sign up')
+#         register_btn.click()
+#         time.sleep(2)
+#         assert self.browser.current_url == 'http://localhost:1667/#/register'
+#
+#         username_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+#         email_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+#         password_input = self.browser.find_element(By.XPATH, '//input[@type="password"]')
+#         sign_up_btn = self.browser.find_element(By.XPATH, '//button[contains(text(), "Sign up")]')
+#
+#         username_input.send_keys('Testuser4')
+#         email_input.send_keys('testuser4@gmail.com')
+#         password_input.send_keys('Testuser1password')
+#         sign_up_btn.click()
+#         time.sleep(2)
+#
+#         registration_popup_title = self.browser.find_element(By.XPATH, '//div[@class="swal-title"]')
+#         registration_popup_msg = self.browser.find_element(By.XPATH, '//div[@class="swal-text"]')
+#         assert registration_popup_title.text == "Welcome!"
+#         assert registration_popup_msg.text == "Your registration was successful!"
+#
+# # TC_03 Bejelentkezés tesztelése
+    def test_login(self):
+        login_btn = self.browser.find_element(By.LINK_TEXT, 'Sign in')
+        login_btn.click()
         time.sleep(2)
         assert self.browser.current_url == 'http://localhost:1667/#/register'
 
-        username_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+        # username_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
         email_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
         password_input = self.browser.find_element(By.XPATH, '//input[@type="password"]')
         sign_up_btn = self.browser.find_element(By.XPATH, '//button[contains(text(), "Sign up")]')
 
-        username_input.send_keys('Testuser3')
-        # username_input.send_keys(testuser["username"]) !!! TESZTADAT IMPORT-ra átírni
-        email_input.send_keys('testuser3@gmail.com')
+        email_input.send_keys('testuser4@gmail.com')
         password_input.send_keys('Testuser1password')
         sign_up_btn.click()
         time.sleep(2)
 
         registration_popup_title = self.browser.find_element(By.XPATH, '//div[@class="swal-title"]')
         registration_popup_msg = self.browser.find_element(By.XPATH, '//div[@class="swal-text"]')
-        assert registration_popup_title.text == "Welcome!"
-        assert registration_popup_msg.text == "Your registration was successful!"
-
-
-# TC_03 Bejelentkezés tesztelése
-# login_btn = browser.find_element(By.LINK_TEXT, 'Sign in')
-# login_btn.click()
-# time.sleep(2)
-# assert browser.current_url == 'http://localhost:1667/#/login'
-
-# TC_04 Adatok listázása
-
-# TC_05 Több oldalas lista bejárása
-
-# TC_06 Új adat bevitel
-
-# TC_07 Ismételt és sorozatos adatbevitel adatforrásból
-
-# TC_08 Meglévő adat módosítás
-
-# TC_09 Adat vagy adatok törlése
-
-# TC_10 Adatok lementése felületről
+        # assert registration_popup_title.text == "Welcome!"
+        # assert registration_popup_msg.text == "Your registration was successful!"
+#
+# # TC_04 Adatok listázása
+#
+# # TC_05 Több oldalas lista bejárása
+#
+# # TC_06 Új adat bevitel
+#
+# # TC_07 Ismételt és sorozatos adatbevitel adatforrásból
+#
+# # TC_08 Meglévő adat módosítás
+#
+# # TC_09 Adat vagy adatok törlése
+#
+# # TC_10 Adatok lementése felületről
 
 # TC_11 Kijelentkezés
