665141be32323b166fe7ad8ef9098a823e2653aa
Papp Eva <ms.eva.papp@gmail.com>
2023-04-13 13:09:20 +0200

    logout

--- test_conduit_vizsgaremek/functions.py
+++ test_conduit_vizsgaremek/functions.py
@@ -1,8 +1,22 @@
+from selenium.webdriver.common.by import By
+import time
+
 # TC_01 Adatkezelési nyilatkozat használata
 
-# TC_02 Regisztráció
+# Bejelentkezés
+def login(browser):
+    menu_login_btn = browser.find_element(By.LINK_TEXT, 'Sign in')
+    menu_login_btn.click()
+    time.sleep(1)
 
-# TC_03 Bejelentkezés
+    email_input = browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+    password_input = browser.find_element(By.XPATH, '//input[@type="password"]')
+    sign_in_btn = browser.find_element(By.XPATH, '//button[contains(text(), "Sign in")]')
+
+    email_input.send_keys('testuser4@gmail.com')
+    password_input.send_keys('Testuser1password')
+    sign_in_btn.click()
+    time.sleep(3)
 
 # TC_04 Adatok listázása
 
