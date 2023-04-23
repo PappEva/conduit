81428a7a4dbcde39a2055b00db3690d862baf06b
Papp Eva <ms.eva.papp@gmail.com>
2023-04-23 17:47:49 +0200

    1-11 done

--- test_conduit_vizsgaremek/test_conduit.py
+++ test_conduit_vizsgaremek/test_conduit.py
@@ -3,11 +3,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
-
 import csv
 from functions import login_function, cookie_function, data_for_delete_function
 from data_for_imports import user_login, article, article_for_delete, bio_data_for_modify
@@ -28,8 +26,7 @@ class TestConduit(object):
         # time.sleep(2)
 
     def teardown_method(self):
-        # self.browser.quit()
-        pass
+        self.browser.quit()
 
     # TC_01 Adatkezelési nyilatkozat használata ########################################################################
     def test_cookie(self):
